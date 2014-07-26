b10df469318f8960bee2879cbb46e8e50049f29a
Runze <ernuzwang@gmail.com>
2014-07-25 23:51:15 -0700

    Update 1. initial processing.R

--- 1. initial processing.R
+++ 1. initial processing.R
@@ -1,7 +1,7 @@
 setwd('/Users/Runze/Github/artsy')
 
 #load followers' profiles (list parsed from json)
-load(file = 'follower_profile.RData')
+load(file = 'data/follower_profile.RData')
 
 #function to only extract relevant info
 extract_info = function(elem) {
@@ -10,7 +10,10 @@ extract_info = function(elem) {
 
 #subset data and transform into data.frame
 follower_profile_sub = lapply(follower_profile, extract_info)
+
+#replace null items to blank (because not all profiles share the same amount of information)
 follower_profile_sub = lapply(follower_profile_sub, lapply, function(x) ifelse(is.null(x), '', x))
+
 followers = data.frame(matrix(unlist(follower_profile_sub), nrow = length(follower_profile), byrow = T))
 names(followers) = names(follower_profile_sub[[1]])
 followers$location = as.character(followers$location)
